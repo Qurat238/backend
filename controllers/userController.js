1419084cd61db662f688de83315487a956b9f6c0
Qurat Ul Ain <116911922+Qurat238@users.noreply.github.com>
2024-05-28 21:27:32 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,22 +5,17 @@ import sendToken from "../utils/jwttoken.js";
 import sendEmail from "../utils/sendEmail.js";
 import crypto from "crypto";
 import cloudinary from "cloudinary";
-
 //Register a user
 export const registerUser = catchAsyncErrors(async(req,res,next) => {
-
     const {name, email, password} = req.body;
     
     let user = await User.findOne({email});
-
     if(user){
         return next(new ErrorHandler("User Already Exists", 400));
     }
 
-    const otp = Math.floor(Math.random() * 1000000);
-
     const avatar = req.files.avatar;
-    const avatarBuffer = avatar.data.toString('base64');
+    const avatarBuffer = avatar.data.toString('base64')
 
     const myCloud = await cloudinary.uploader.upload(`data:${avatar.mimetype};base64,${avatarBuffer}`, { resource_type: 'auto' }, (error, result) => {
         if (error) {
@@ -29,7 +24,6 @@ export const registerUser = catchAsyncErrors(async(req,res,next) => {
           console.log('Image uploaded successfully:', result);
         }
       });
-
     user = await User.create({
         name,
         email,
@@ -38,70 +32,29 @@ export const registerUser = catchAsyncErrors(async(req,res,next) => {
             public_id: myCloud.public_id,
             url: myCloud.secure_url,
         },
-        otp,
-        otp_expiry: new Date(Date.now() + process.env.OTP_EXPIRE * 60 * 1000),
+
     });
 
-    const message = `Your OTP is ${otp}`;
-
-    try {
-        await sendEmail({
-            email:user.email,
-            subject:`Verify your account`,
-            message
-        });
-        sendToken(user,201,res, "OTP sent to your email, please verify your account");
-    } catch (error) {
-        await user.save({validateBeforeSave:false});
-        return next(new ErrorHandler(error.message, 500));
-    }
-
+   sendToken(user,201,res);
 });
-
-
-// Verify
-export const verify = async (req, res) => {
-      const otp = Number(req.body.otp);
-      const user = await User.findById(req.user.id);
-  
-      if (user.otp !== otp || user.otp_expiry < Date.now()) {
-        return res
-          .status(400)
-          .json({ success: false, message: "Invalid OTP or has been Expired" });
-      }
-  
-      user.verified = true;
-      user.otp = null;
-      user.otp_expiry = null;
-  
-      await user.save();
-  
-      sendToken(user, 200, res, "Account Verified");
-  };
-
 
 //Login a user
 export const loginUser = catchAsyncErrors(async(req,res,next) => {
     const { email, password } = req.body;
-
     //Checkin if user has given email and password both
     if(!email || !password){
         return next(new ErrorHandler("Please enter email and pasword", 400));
     }
-
     const user = await User.findOne({email}).select("+password");
-
     if(!user){
         return next(new ErrorHandler("Invalid Email or Password", 401));
     }
-
     const isPasswordMatched = await user.comparePassword(password);
-
     if(!isPasswordMatched){
         return next(new ErrorHandler("Invalid Email or Password", 401));
     }
 
-    sendToken(user, 200, res, "Logged In Successfully");
+    sendToken(user, 200, res);
 });
 
 //Logout a user
@@ -110,99 +63,91 @@ export const logout = catchAsyncErrors(async(req,res,next) => {
         expires: new Date(Date.now()),
         httpOnly:true
     });
-
     res.status(200).json({
         success:true,
         message:"Logged Out"
     });
 });
-
 //Forgot Pasword
 export const forgotPassword = catchAsyncErrors(async(req,res,next) => {
     const user = await User.findOne({email: req.body.email});
     if(!user){
         return next(new ErrorHandler("User not found", 404));
     }
+    //Get Reset Password Token
+    const resetToken = user.getResetPasswordToken();
+    await user.save({validateBeforeSave:false});
 
-    const otp = Math.floor(Math.random() * 1000000);
-
-    user.resetPasswordOtp = otp;
-    user.resetPasswordOtpExpiry = Date.now() + 10 * 60 * 1000;
-
-    await user.save();
-
-    const message = `Your OTP for reseting the password ${otp}. If you did not request for this, please ignore this email.`;
-
-    await sendEmail({email:user.email, subject:"Request for Reseting Password", message});
-
-    sendToken(user,200,res, `OTP sent to ${email}`);
+    const resetPasswordUrl = `https://backend-gray-sigma.vercel.app/password/reset/${resetToken}`;
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email, then please just ignore it.`;
 
     try {
         await sendEmail({
             email:user.email,
-            subject:`Request for Reseting Password`,
+            subject:`Ecommerce Password Recovery`,
             message
         });
-        sendToken(user,200,res, `OTP sent to ${email}`);
+        res.status(200).json({
+            success:true,
+            message:`Email sent to ${user.email} successfully`
+        });
     } catch (error) {
-        user.resetPasswordOtp = undefined;
-        user.resetPasswordOtpExpiry = undefined;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
         await user.save({validateBeforeSave:false});
         return next(new ErrorHandler(error.message, 500));
+
     }
 });
 
 //Reset Passowrd
 export const resetPassword = catchAsyncErrors(async(req,res,next) => {
 
-    const { otp, newPassword } = req.body;
-
     //Creating token hashed
     const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex");
 
     const user = await User.findOne({
-        resetPasswordOtp: otp,
-        resetPasswordOtpExpiry: { $gt:Date.now() }
+        resetPasswordToken,
+        resetPasswordExpire: { $gt:Date.now() }
     });
-    
+
     if(!user){
-        return next(new ErrorHandler("Reset Password Otp in invalid or has been expired", 400));
+        return next(new ErrorHandler("Reset Password Token in invalid or has been expired", 400));
     }
 
-    user.password = newPassword;
-    user.resetPasswordOtp = null;
-    user.resetPasswordOtpExpiry = null;
+    if(req.body.password !== req.body.confirmPassword){
+        return next(new ErrorHandler("Password does not match", 400));
+    }
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
     await user.save();
-    sendToken(user,200,res, "Password Changed Successfully");
+    sendToken(user,200,res);
 });
 
 //Get User Detail
 export const getUserDetails = catchAsyncErrors(async(req,res,next) => {
     const user = await User.findById(req.user.id);
-
     res.status(200).json({
         success:true,
         user
     });
 });
-
 //Update User Password
 export const updatePassword = catchAsyncErrors(async(req,res,next) => {
     const user = await User.findById(req.user.id).select("+password");
     
     const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
-
     if(!isPasswordMatched){
         return next(new ErrorHandler("Old password is incorrect", 400));
     }
-
     if(req.body.newPassword !== req.body.confirmPassword){
         return next(new ErrorHandler("confirm password does not matched", 400));
     }
     user.password = req.body.newPassword;
     await user.save();
 
-    sendToken(user, 200, res, "Password Updated Successfully");
+    sendToken(user, 200, res);
 });
 
 //Update User Profile
@@ -211,14 +156,12 @@ export const updateProfile = catchAsyncErrors(async(req,res,next) => {
         name: req.body.name,
         email: req.body.email
     }
-
     if(req.body.avatar !== ""){
         const user = await User.findById(req.user.id);
         const imageId = user.avatar.public_id;
         await cloudinary.v2.uploader.destroy(imageId);
         const avatar = req.files.avatar;
     const avatarBuffer = avatar.data.toString('base64')
-
     const myCloud = await cloudinary.uploader.upload(`data:${avatar.mimetype};base64,${avatarBuffer}`, { resource_type: 'auto' }, (error, result) => {
         if (error) {
           console.error('Error uploading image to Cloudinary:', error);
@@ -236,12 +179,10 @@ export const updateProfile = catchAsyncErrors(async(req,res,next) => {
         runValidators:true,
         useFindAndModify:false
     });
-
     res.status(200).json({
         success:true
     });
 });
-
 //Get All Users (Admin)
 export const getAllUsers = catchAsyncErrors(async(req,res,next) => {
     const users = await User.find();
@@ -250,7 +191,6 @@ export const getAllUsers = catchAsyncErrors(async(req,res,next) => {
         users
     });
 });
-
 //Get Single User (Admin)
 export const getSingleUser = catchAsyncErrors(async(req,res,next) => {
     const user = await User.findById(req.params.id);
@@ -262,7 +202,6 @@ export const getSingleUser = catchAsyncErrors(async(req,res,next) => {
         user
     });
 });
-
 //Update Role (Admin)
 export const updateUserRole = catchAsyncErrors(async(req,res,next) => {
     const newUserData = {
@@ -282,7 +221,6 @@ export const updateUserRole = catchAsyncErrors(async(req,res,next) => {
         success:true
     });
 });
-
 //Delete User (Admin)
 export const deleteUser = catchAsyncErrors(async(req,res,next) => {
     const user = await User.findById(req.params.id);
@@ -297,4 +235,3 @@ export const deleteUser = catchAsyncErrors(async(req,res,next) => {
         message:"User Deleted successfully"
     });
 });
-
