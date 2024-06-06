c7b05afc2d6f1f04161469938fbf2f62eab3ecf9
Qurat Ul Ain <116911922+Qurat238@users.noreply.github.com>
2024-06-05 22:38:17 -0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -61,14 +61,4 @@ userSchema.methods.getJWTToken = function (){
 userSchema.methods.comparePassword = async function(enteredPassword){
     return await bcryptjs.compare(enteredPassword, this.password);
 }
-//Generating Password Reset Token
-userSchema.methods.getResetPasswordToken = function(){
-    //Generating Token
-    const resetToken = crypto.randomBytes(20).toString("hex");
-    
-    //Hashing and adding resetPasswordToken in userSchema
-    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
-    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
-    return resetToken;
-}
 export default mongoose.model("User", userSchema);
