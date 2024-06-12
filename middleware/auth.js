71962489639654621f16bcf7bbe50096522d298c
Qurat Ul Ain <116911922+Qurat238@users.noreply.github.com>
2024-06-12 15:26:32 +0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,10 @@ import User from "../models/userModel.js";
 
 export const isAuthenticatedUser = catchAsyncErrors(async(req,res,next) => {
     const { token } = req.cookies;
+
+    if(!token){
+        return next(new ErrorHandler("Please login to access this resource", 401));
+    }
     
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
